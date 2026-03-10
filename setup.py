82dd4efc05e64345873ac539d413ff3eeb0fcb77
DimenCoder <renangameslinha8@gmail.com>
2026-03-10 17:01:18 -0300

    Criar setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="notify",
+    version="1.0.0",
+    description="Simple Termux notification library",
+    author="Inferno-God1001",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8"
+)
