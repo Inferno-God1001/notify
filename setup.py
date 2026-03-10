6fb632a276a1c2f273a0e8429e4834e9dbdc5b38
DimenCoder <renangameslinha8@gmail.com>
2026-03-10 17:21:48 -0300

    Bump version from 1.0.1 to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="notify",
-    version="1.0.1",
+    version="1.0.2",
     description="Simple Termux notification library",
     author="Inferno-God1001",
     packages=find_packages(),
